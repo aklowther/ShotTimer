218000e8f5230d9c632bb0083d357720ae669f26
Adam Lowther <adamklowther@gmail.com>
2015-05-28 16:13:08 -0400

    Initial Commit

---
+++ Dry Fire Timer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dry Fire Timer
+//
+//  Created by Adam Lowther on 5/28/15.
+//  Copyright (c) 2015 Lowther. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
