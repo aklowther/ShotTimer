846076afc915878e38a48df80386549259e85ec7
Adam Lowther <adamklowther@gmail.com>
2015-05-28 17:11:25 -0400

    initial commit

--- Dry Fire Timer/ViewController.swift
+++ Dry Fire Timer/ViewController.swift
@@ -7,11 +7,22 @@
 //
 
 import UIKit
+import AudioToolbox
+import AVFoundation
 
 class ViewController: UIViewController {
+    @IBOutlet weak var startStopButton: UIButton!
 
+//    var isStopped = false
+    var audioPlayer = AVAudioPlayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.startStopButton.setTitle("Start", forState: UIControlState.Normal)
+        self.startStopButton.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
+        self.startStopButton.layer.cornerRadius = 0.5 * self.startStopButton.bounds.size.width
+        self.startStopButton.backgroundColor = UIColor.greenColor()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -21,5 +32,34 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func startStopButtonPressed(sender: AnyObject) {
+        self.startStopButton.setTitle("Make Ready", forState: UIControlState.Normal)
+        self.startStopButton.backgroundColor = UIColor.purpleColor()
+        self.startStopButton.userInteractionEnabled = false
+        self.randomTimerFireBuzzer()
+        
+//        self.isStopped = !self.isStopped
+    }
+    
+    func randomTimerFireBuzzer() {
+        let mini = UInt32(2)
+        let maxi = UInt32(4)
+            
+        let delay = Double(mini + arc4random_uniform(maxi - mini))
+        
+        var timer = NSTimer.scheduledTimerWithTimeInterval(delay, target: self, selector: Selector("soundBuzzer"), userInfo: nil, repeats: false)
+        var coinSound = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("Glass", ofType: "aiff")!)
+//        var audioPlayer = AVAudioPlayer()
+        audioPlayer = AVAudioPlayer(contentsOfURL: coinSound, error: nil)
+        audioPlayer.prepareToPlay()
+    }
+    
+    func soundBuzzer() {
+        audioPlayer.play()
+        
+        self.startStopButton.setTitle("Start", forState: UIControlState.Normal)
+        self.startStopButton.backgroundColor = UIColor.greenColor()
+        self.startStopButton.userInteractionEnabled = true
+    }
 }
 
